e232aa297608c04c1ad024a663ec414387b95a63
angelkim88 <66551410+haron47@users.noreply.github.com>
2021-09-25 06:34:22 +0900

    start 쉘코드 변경

--- code_deploy/after_install.sh
+++ code_deploy/after_install.sh
@@ -2,4 +2,9 @@
 
 cd /home/ec2-user/app
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+node -e "console.log('Running Node.js ' + process.version)"
+
 sudo rm -rf node_modules
