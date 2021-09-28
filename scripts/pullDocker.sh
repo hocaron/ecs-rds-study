5481f3c063aa037dfaf21577887035c787a224c9
angelkim88 <66551410+haron47@users.noreply.github.com>
2021-09-29 01:06:49 +0900

    docker 스크립트 작성

--- scripts/pullDocker.sh
+++ scripts/pullDocker.sh
@@ -1,12 +1,20 @@
-docker login -u ssunnu -p testtesttest
-# pull docker image
-if [ "$DEPLOYMENT_GROUP_NAME" == "helltabus-codedeploy-g" ]
+
+if [ "$DEPLOYMENT_GROUP_NAME" == "dev" ]
 then
-docker-compose -f /deploy/docker-compose.dev.yml pull
-elif [ "$DEPLOYMENT_GROUP_NAME" == "helltabus-codedeploy" ]
+# Remove any anonymous volumes attached to containers
+docker-compose -f /deploy/docker-compose.dev.yml rm -v
+# build images and run containers
+docker-compose -f /deploy/docker-compose.dev.yml up --detach --renew-anon-volumes
+elif [ "$DEPLOYMENT_GROUP_NAME" == "stage" ]
 then
-docker-compose -f /deploy/docker-compose.stage.yml pull
+# Remove any anonymous volumes attached to containers
+docker-compose -f /deploy/docker-compose.stage.yml rm -v
+# build images and run containers
+docker-compose -f /deploy/docker-compose.stage.yml up --detach --renew-anon-volumes
 elif [ "$DEPLOYMENT_GROUP_NAME" == "production" ]
 then
-docker-compose -f /deploy/docker-compose.yml pull
+# Remove any anonymous volumes attached to containers
+docker-compose -f /deploy/docker-compose.yml rm -v
+# build images and run containers
+docker-compose -f /deploy/docker-compose.yml up --detach --renew-anon-volumes
 fi
