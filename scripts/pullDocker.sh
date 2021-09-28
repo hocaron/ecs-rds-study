6a4386118d25ca58d62dece5760d5ad118bfdeca
angelkim88 <66551410+haron47@users.noreply.github.com>
2021-09-29 04:19:14 +0900

    docker 스크립트 작성

--- scripts/pullDocker.sh
+++ scripts/pullDocker.sh
@@ -1,20 +1,14 @@
+# docker login
+docker login -u ssunnu -p testtesttest
 
+# pull docker image
 if [ "$DEPLOYMENT_GROUP_NAME" == "dev" ]
 then
-# Remove any anonymous volumes attached to containers
-docker-compose -f /deploy/docker-compose.dev.yml rm -v
-# build images and run containers
-docker-compose -f /deploy/docker-compose.dev.yml up --detach --renew-anon-volumes
+  docker-compose -f /deploy/docker-compose.dev.yml pull
 elif [ "$DEPLOYMENT_GROUP_NAME" == "stage" ]
 then
-# Remove any anonymous volumes attached to containers
-docker-compose -f /deploy/docker-compose.stage.yml rm -v
-# build images and run containers
-docker-compose -f /deploy/docker-compose.stage.yml up --detach --renew-anon-volumes
+  docker-compose -f /deploy/docker-compose.stage.yml pull
 elif [ "$DEPLOYMENT_GROUP_NAME" == "production" ]
 then
-# Remove any anonymous volumes attached to containers
-docker-compose -f /deploy/docker-compose.yml rm -v
-# build images and run containers
-docker-compose -f /deploy/docker-compose.yml up --detach --renew-anon-volumes
+  docker-compose -f /deploy/docker-compose.yml pull
 fi
