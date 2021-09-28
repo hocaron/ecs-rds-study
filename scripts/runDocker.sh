eb9abb502b0d5df8034be6c40ef816928b0deaaf
angelkim88 <66551410+haron47@users.noreply.github.com>
2021-09-29 00:49:55 +0900

    docker 스크립트 작성

--- scripts/runDocker.sh
+++ scripts/runDocker.sh
@@ -1,11 +1,11 @@
 
-if [ "$DEPLOYMENT_GROUP_NAME" == "helltabus-codedeploy-g" ]
+if [ "$DEPLOYMENT_GROUP_NAME" == "helltabus-codedeploy" ]
 then
 # Remove any anonymous volumes attached to containers
 docker-compose -f /deploy/docker-compose.dev.yml rm -v
 # build images and run containers
 docker-compose -f /deploy/docker-compose.dev.yml up --detach --renew-anon-volumes
-elif [ "$DEPLOYMENT_GROUP_NAME" == "helltabus-codedeploy-g" ]
+elif [ "$DEPLOYMENT_GROUP_NAME" == "helltabus-codedeploy" ]
 then
 # Remove any anonymous volumes attached to containers
 docker-compose -f /deploy/docker-compose.stage.yml rm -v
