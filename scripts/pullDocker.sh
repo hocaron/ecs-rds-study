5345618b42909ee03448ca1377693a57b588cfa7
angelkim88 <66551410+haron47@users.noreply.github.com>
2021-09-29 00:38:38 +0900

    docker 스크립트 경로 변경

--- scripts/pullDocker.sh
+++ scripts/pullDocker.sh
@@ -1,6 +1,6 @@
 docker login -u ssunnu -p testtesttest
 # pull docker image
-if [ "$DEPLOYMENT_GROUP_NAME" == "helltabus-codedeploy" ]
+if [ "$DEPLOYMENT_GROUP_NAME" == "helltabus-codedeploy-g" ]
 then
 docker-compose -f /deploy/docker-compose.dev.yml pull
 elif [ "$DEPLOYMENT_GROUP_NAME" == "helltabus-codedeploy" ]
--- scripts/runDocker.sh
+++ scripts/runDocker.sh
@@ -1,5 +1,5 @@
 
-if [ "$DEPLOYMENT_GROUP_NAME" == "helltabus-codedeploy" ]
+if [ "$DEPLOYMENT_GROUP_NAME" == "helltabus-codedeploy-g" ]
 then
 # Remove any anonymous volumes attached to containers
 docker-compose -f /deploy/docker-compose.dev.yml rm -v
@@ -11,10 +11,4 @@ then
 docker-compose -f /deploy/docker-compose.stage.yml rm -v
 # build images and run containers
 docker-compose -f /deploy/docker-compose.stage.yml up --detach --renew-anon-volumes
-elif [ "$DEPLOYMENT_GROUP_NAME" == "production" ]
-then
-# Remove any anonymous volumes attached to containers
-docker-compose -f /deploy/docker-compose.yml rm -v
-# build images and run containers
-docker-compose -f /deploy/docker-compose.yml up --detach --renew-anon-volumes
-fi
+
